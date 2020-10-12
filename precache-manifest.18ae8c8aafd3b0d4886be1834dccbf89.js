29986544aa59e1a197aa84824e6f7bb7a8bc3137
Priyalcitrusbug <priyal.citrusbug@gmail.com>
2020-10-12 10:15:16 +0530

    Updates

---
+++ precache-manifest.18ae8c8aafd3b0d4886be1834dccbf89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba283776b3da322799b5a2e73dfeab7",
+    "url": "/Blog-Redux-App/index.html"
+  },
+  {
+    "revision": "fed6248076d8207a1dba",
+    "url": "/Blog-Redux-App/static/css/2.50f53c61.chunk.css"
+  },
+  {
+    "revision": "035e485bb138b3dc6537",
+    "url": "/Blog-Redux-App/static/css/main.53a11a5c.chunk.css"
+  },
+  {
+    "revision": "fed6248076d8207a1dba",
+    "url": "/Blog-Redux-App/static/js/2.ee36d4bb.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/Blog-Redux-App/static/js/2.ee36d4bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "035e485bb138b3dc6537",
+    "url": "/Blog-Redux-App/static/js/main.1141ad23.chunk.js"
+  },
+  {
+    "revision": "1b4804dd39ea6852bee1",
+    "url": "/Blog-Redux-App/static/js/runtime-main.248439e1.js"
+  }
+]);
